eab51fddfc9d0662a1de121904f2eba225f65560
ndandega <ndandega@DIN51003307M.corp.capgemini.com>
2019-06-26 16:29:48 +0530

    init by me

---
+++ Addition/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2,b=3,c;
+		c=a+b;
+		System.out.println("Addition is="+c);
+
+	}
+
+}
