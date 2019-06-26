1b707d1aa7995c3f939ba284e5733fe18ca90ba5
ndandega <ndandega@DIN51003307M.corp.capgemini.com>
2019-06-26 16:21:22 +0530

    init by me

---
+++ decimal/src/demo.java
@@ -0,0 +1,10 @@
+ 
+
+import java.util.*; 
+import java.util.function.BinaryOperator;
+public class demo {
+	public static void main(String[] args) {
+	String n="1010";
+	int decimal=Integer.parseInt(n,2);
+	System.out.println(decimal);
+}}
